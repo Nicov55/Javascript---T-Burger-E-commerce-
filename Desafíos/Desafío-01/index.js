c1679d0af9723dda139b7e9b0f37de69e121c31a
Nicolas Verón <nveronpf@gmail.com>
2022-06-15 23:14:43 -0300

    Turnera Base

--- Desafíos/Desafío-01/index.js
+++ Desafíos/Desafío-01/index.js
@@ -1,1 +1,21 @@
 alert ("Sistema de Turnos");
+for (let i = 1; i<50; i++) {
+
+    let nombre = prompt ("Ingrese su nombre");
+    let apellido = prompt ("Ingrese su apellido");
+
+	while ((nombre=="") || (apellido=="")) {
+		alert ("Error: Ingresar Nombre y Apellido"); 
+        nombre = prompt ("Ingrese su nombre");
+        apellido = prompt ("Ingrese su apellido");
+    }
+	alert (" Nombre "+ " " + nombre +" - Apellido "+ " " + apellido);
+	
+    alert (" Turno N° " + i + " " + nombre + " " + apellido);
+
+	if(i === 5)
+    {   alert (" Se ha llegado al límite de turnos disponibles en el día, intente nuevamente mañana");
+		break;
+	}
+
+}
