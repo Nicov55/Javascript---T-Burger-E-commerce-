0292b0636a95ad7663255513dadb0e5ce0988ccf
Nicolas Verón <nveronpf@gmail.com>
2022-06-15 23:28:58 -0300

    Desafio Completo

--- Desafíos/Desafío-01/index.js
+++ Desafíos/Desafío-01/index.js
@@ -1,4 +1,6 @@
 alert ("Sistema de Turnos");
+
+
 for (let i = 1; i<50; i++) {
 
     let nombre = prompt ("Ingrese su nombre");
@@ -19,3 +21,34 @@ for (let i = 1; i<50; i++) {
 	}
 
 }
+
+
+let turno = parseInt (prompt ("Ingrese su N° de turno"));
+
+if (turno > 5) {
+    alert ("Su turno no corresponde al día asignado");}
+
+else {};
+
+let horario = prompt ("Ingrese el horario de atención (Mañana - Tarde - Vespertino)").toUpperCase ();
+
+switch (horario) {
+    case "MAÑANA":
+        alert("Muchas Gracias, recibira un correo con su turno en el horario de la MAÑANA");
+        break;
+        
+    case "TARDE":
+        alert("Muchas Gracias, recibira un correo con su turno en el horario de la TARDE");
+        break;
+
+    case "VESPERTINO":
+        alert("Muchas Gracias, recibira un correo con su turno en el horario VESPERTINO");
+        break;
+
+    default:
+        alert("Error, vuelva a intentarlo nuevamente");
+        break;
+        
+}
+
+
