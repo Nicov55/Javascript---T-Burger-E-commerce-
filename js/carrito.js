11875797afb5cef69f24c47fba2d40bdcc41e26e
Nicolas Verón <nveronpf@gmail.com>
2022-07-29 11:56:02 -0300

    Responsive

---
+++ js/carrito.js
@@ -0,0 +1,39 @@
+
+// ------- CARRITO ---------
+
+let carrito = [];
+
+const mainCarrito = document.querySelector ('.maincarrito')
+
+let cantidadPedido = document.getElementById('cantidadpedido');
+
+let cantidadPedidoCarrito = document.getElementById('cantidadpedidocarrito');
+
+
+i=0;
+
+let clicBurger = document.getElementsByClassName("botonagregar");
+
+alertaAgregarAlCarrito();
+
+// Carrito Pre HTML / Carrito Page HTML / Librerias
+
+// PARA FINALIZAR CARRITO EN NUEVO HTML, TOMAR INDICE DE LS
+
+// MODAL BS - Adicionales y Carrito
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
